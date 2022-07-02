8310dfadbda30a66e5861f73ad8a77db346c6a8f
Sunny George <noisiv807@gmail.com>
2022-07-02 08:46:40 -0700

    my commit

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+
+    length = len(my_last)
+
+    if idx > length - 1:
+        return (my_list)
+
+    my_list[idx] = element
+
+    return (my_list)
