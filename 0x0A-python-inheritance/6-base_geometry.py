5edc3dd3091804cb241617e4c1dda724b95f7278
Sunny George <noisiv807@gmail.com>
2022-08-01 13:35:38 -0700

    Integer validator

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -9,5 +9,5 @@ class BaseGeometry():
 
     def area(self):
         """Definition of area method.
-            Raises an Exception with message 'area() is not implemented'"""
+           Raises an Exception with message 'area() is not implemented'"""
         raise Exception("area() is not implemented")
