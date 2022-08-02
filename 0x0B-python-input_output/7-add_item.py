3e7f5ea50584c04793955677c03a4eb23720ff5c
Sunny George <noisiv807@gmail.com>
2022-08-02 15:12:23 -0700

    task update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-""" takes in argument and write it to a json file"""
-import sys
-from os.path import exists
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save = __import__('5-save_to_json_file').save_to_json_file
+"""
+script appends items to a list in
+the add_item.json file
+"""
 
 
-if len(sys.argv) > 1:
-    if exists("add_item.json"):
-        arr = load_from_json_file("add_item.json")
-    else:
-        arr = []
-    for i in (sys.argv[1:]):
-        arr.append(i)
-    save(arr, "add_item.json")
-elif not exists("add_item.json"):
-    save([], "add_item.json")
+if __name__ == "__main__":
+    save = __import__("5-save_to_json_file").save_to_json_file
+    load = __import__("6-load_from_json_file").load_from_json_file
+    from sys import argv
+
+    try:
+        obj = load("add_item.json")
+
+    except FileNotFoundError:
+        obj = []
+        save(obj, "add_item.json")
+    i = 1
+
+    while i < len(argv):
+        obj.append(argv[i])
+        i += 1
+    save(obj, "add_item.json")
