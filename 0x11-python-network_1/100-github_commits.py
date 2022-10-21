ad43c9d9b7fd0b9ad5771d960e3146f9b2394e18
Sunny George <noisiv807@gmail.com>
2022-10-21 11:47:10 -0700

    update

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module lists the last 10 commits from a github repo
+"""
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    repo = argv[1]
+    owner = argv[2]
+
+    response = requests.get(
+        f"https://api.github.com/repos/{owner}/{repo}/commits",
+    )
+    commits = response.json()
+    i = 0
+    try:
+        while i < 10:
+            co = commits[i]
+            print(
+                f"{co.get('sha')}: {co.get('commit').get('author').get('name')}"
+            )
+            i += 1
+    except IndexError:
+        pass
